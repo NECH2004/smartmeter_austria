3b10e23468d412fea2c9f4a96c82a1c6a15678cc
Christian Neumeier <47736781+NECH2004@users.noreply.github.com>
2024-01-29 19:48:00 +0100

    Change version information to 1.4.6

--- custom_components/smartmeter_austria/const.py
+++ custom_components/smartmeter_austria/const.py
@@ -26,7 +26,7 @@ PLATFORMS = [Platform.SENSOR]
 
 # Additional
 """The actual version of the integration."""
-VERSION = "1.4.5"
+VERSION = "1.4.6"
 
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
